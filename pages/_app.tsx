9c6d6f6066476d4c44bc8a0cd61ca775181ac987
Tony He <tony.hlp@hotmail.com>
2023-04-19 19:02:20 -0400

    feat: add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,26 +3,28 @@ import "remixicon/fonts/remixicon.css";
 import Head from "next/head";
 import Footer from "../components/Footer";
 import NextNprogress from "nextjs-progressbar";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <div>
-      <Head>
-        <title>Known Unknowns | 已知未知 Podcast</title>
-        <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎙️</text></svg>"
-        ></link>
-      </Head>
-      <NextNprogress
-        color="#D1D5DB"
-        height={2}
-        options={{ showSpinner: false }}
-      />
-      <Component {...pageProps} />
-      <Footer />
-    </div>
-  );
+	return (
+		<div>
+			<Head>
+				<title>Known Unknowns | 已知未知 Podcast</title>
+				<link
+					rel='icon'
+					href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎙️</text></svg>'
+				></link>
+			</Head>
+			<NextNprogress
+				color='#D1D5DB'
+				height={2}
+				options={{ showSpinner: false }}
+			/>
+			<Component {...pageProps} />
+			<Footer />
+			<Analytics />
+		</div>
+	);
 }
 
 export default MyApp;
