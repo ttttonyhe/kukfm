610e890397343e96170697e40433c8e9061f1fbe
Tony He <tony.hlp@hotmail.com>
2023-04-19 19:03:29 -0400

    fix: type error

--- components/Aside.tsx
+++ components/Aside.tsx
@@ -2,41 +2,42 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Aside() {
-  return (
-    <aside>
-      <summary className="mb-5 lg:mb-0 w-full bg-white shadow-sm border rounded-xl justify-center items-center p-6 py-8 text-center">
-        <Image
-          className="mx-auto rounded-xl"
-          src="https://static.ouorz.com/wp-content/uploads/2020/12/2021010105320083.jpg"
-          width="170"
-          height="170"
-        />
-        <h1 className="mt-4 text-2xl font-medium whitespace-nowrap tracking-wide">
-          Known Unknowns
-        </h1>
-        <p className="text-lg text-gray-500 tracking-wide">已知未知播客</p>
-      </summary>
-      <Link href="/">
-        <div className="text-gray-600 mt-6 mb-2 bg-white shadow-sm border rounded-xl tracking-wider justify-center items-center flex p-2 text-lg cursor-pointer hover:bg-gray-50">
-          <i className="ri-home-3-line mr-1"></i>
-          Home
-        </div>
-      </Link>
-      <Link href="/rss">
-        <div className="text-gray-600 mt-3 mb-2 bg-white shadow-sm border rounded-xl tracking-wider justify-center items-center flex p-2 text-lg cursor-pointer hover:bg-gray-50">
-          <i className="ri-rss-fill mr-1"></i> RSS
-        </div>
-      </Link>
-      <a href="mailto:hi@kukfm.com">
-        <div className="text-gray-600 mt-3 mb-2 bg-white shadow-sm border rounded-xl tracking-wider justify-center items-center flex p-2 text-lg cursor-pointer hover:bg-gray-50">
-          <i className="ri-mail-line mr-1"></i> Email
-        </div>
-      </a>
-      <a href="https://afdian.net/@kukfm" target="_blank" rel="noreferrer">
-        <div className="text-gray-600 mt-3 mb-6 lg:mb-2 bg-white shadow-sm border rounded-xl tracking-wider justify-center items-center flex p-2 text-lg cursor-pointer hover:bg-gray-50">
-          <i className="ri-hand-heart-line mr-1"></i> Sponsor
-        </div>
-      </a>
-    </aside>
-  );
+	return (
+		<aside>
+			<summary className='mb-5 lg:mb-0 w-full bg-white shadow-sm border rounded-xl justify-center items-center p-6 py-8 text-center'>
+				<Image
+					className='mx-auto rounded-xl'
+					src='https://static.ouorz.com/wp-content/uploads/2020/12/2021010105320083.jpg'
+					width='170'
+					height='170'
+					alt='Known Unknowns'
+				/>
+				<h1 className='mt-4 text-2xl font-medium whitespace-nowrap tracking-wide'>
+					Known Unknowns
+				</h1>
+				<p className='text-lg text-gray-500 tracking-wide'>已知未知播客</p>
+			</summary>
+			<Link href='/'>
+				<div className='text-gray-600 mt-6 mb-2 bg-white shadow-sm border rounded-xl tracking-wider justify-center items-center flex p-2 text-lg cursor-pointer hover:bg-gray-50'>
+					<i className='ri-home-3-line mr-1'></i>
+					Home
+				</div>
+			</Link>
+			<Link href='/rss'>
+				<div className='text-gray-600 mt-3 mb-2 bg-white shadow-sm border rounded-xl tracking-wider justify-center items-center flex p-2 text-lg cursor-pointer hover:bg-gray-50'>
+					<i className='ri-rss-fill mr-1'></i> RSS
+				</div>
+			</Link>
+			<a href='mailto:hi@kukfm.com'>
+				<div className='text-gray-600 mt-3 mb-2 bg-white shadow-sm border rounded-xl tracking-wider justify-center items-center flex p-2 text-lg cursor-pointer hover:bg-gray-50'>
+					<i className='ri-mail-line mr-1'></i> Email
+				</div>
+			</a>
+			<a href='https://afdian.net/@kukfm' target='_blank' rel='noreferrer'>
+				<div className='text-gray-600 mt-3 mb-6 lg:mb-2 bg-white shadow-sm border rounded-xl tracking-wider justify-center items-center flex p-2 text-lg cursor-pointer hover:bg-gray-50'>
+					<i className='ri-hand-heart-line mr-1'></i> Sponsor
+				</div>
+			</a>
+		</aside>
+	);
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,10 @@
 export default function Footer() {
-  return (
-    <footer className="text-center text-gray-500 font-light tracking-wide mb-10 mt-10 px-6">
-      <p>
-        &copy; 2020 - {new Date().getFullYear()} 已知未知 Known Unknowns Podcast ·{" "}
-        <a href="https://twitter.com/ttttonyhe">@ttttonyhe</a>
-      </p>
-    </footer>
-  );
+	return (
+		<footer className='text-center text-gray-500 font-light tracking-wide mb-10 mt-10 px-6'>
+			<p>
+				&copy; 2020 - {new Date().getFullYear()} 已知未知 Known Unknowns Podcast
+				· <a href='https://twitter.com/ttttonyhe'>@ttttonyhe</a>
+			</p>
+		</footer>
+	);
 }
